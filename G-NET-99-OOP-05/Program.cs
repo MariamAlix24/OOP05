9506210fd4afbb4ba8947a5d016c6491188e4e74
MariamAlix24 <alimariem59@gmail.com>
2026-09-23 23:46:15 +0300

    Q1  Object Copying

--- G-NET-99-OOP-05/Program.cs
+++ G-NET-99-OOP-05/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-
+            #region Part 01 — Theoretical Questions
+            #region Question1
+            //a)You copy the reference (memory address) of the object, not the object itself. Both variables now point to the exact same object in memory.
+            //b)No. No new memory is allocated on the heap. Any change made through one variable will affect the other because both refer to the same underlying object.
+            /*c)Copying a reference: Copies only the memory pointer. Both variables share and modify the same instance.
+            Copying an object (Cloning): Creates a brand-new instance in memory with duplicate data. Modifying the new object does not affect the original.*/
+            #endregion
+            #endregion
         }
     }
 }
